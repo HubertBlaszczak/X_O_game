40a153cbee51c3fb6e7e9478ca6bd56b28be40ca
Hubert Blaszczak <hubert2393@gmail.com>
2023-08-24 23:31:12 +0200

    Add project files.

---
+++ X_O_game/Board.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace X_O_game
+{
+    internal class Board
+    {
+        public static void drawBoard()
+        {
+            string[,] board = new string [3,3];
+            int fieldNumber = 1;
+            for (int i = 0;  i < board.GetLength(0); i++)
+            {
+                
+                for (int j = 0; j < board.GetLength(1); j++)
+                {
+                    
+                    board[i, j] = fieldNumber.ToString();
+                    fieldNumber++;
+                    Console.Write("  " + board[i, j] + "  ");
+                }
+                Console.WriteLine();
+            }
+            
+        }          
+       
+
+    }
+}
---
+++ X_O_game/Program.cs
@@ -0,0 +1,11 @@
+﻿using X_O_game;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Board board = new Board();
+        Board.drawBoard();
+        
+    }
+}
